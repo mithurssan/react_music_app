b13da5eb0f95831fc90137a2a5efa42baca8e58f
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-05-15 17:15:41 +0100

    add a controlled form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import artist from './assets/artist'
 
@@ -47,16 +47,64 @@ function ArtistMusicDetails({ idx }) {
   )
 }
 
+function UserSurveyForm() {
+  const [name, setName] = useState("");
+  const [comment, setComment] = useState("");
+  const [rating, setRating] = useState("");
+  const [userData, setUserData] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { name, comment, rating };
+    setUserData([...userData, data])
+    alert("Submitted!");
+    setName("");
+    setComment("");
+    setRating("");
+  }
+
+  return (
+    <>
+      <div className="form-container">
+        <h2>Rate this Artist and leave a comment!</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-box">
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" placeholder="name" value={name} onChange={e => setName(e.target.value)} />
+          </div>
+          <div className="form-box">
+            <label htmlFor="comment">Comment:</label>
+            <textarea name="comment" placeholder="comment" rows="10" value={comment} onChange={e => setComment(e.target.value)} />
+          </div>
+          <div className="form-box">
+            <label htmlFor="selectedRating"> Rating:</label>
+            <select name="selectedRating" value={rating} onChange={e => setRating(e.target.value)}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  )
+
+}
+
 function App() {
   const songComponents = [];
   for (let i = 0; i < artist.music.length; i++) {
-    songComponents.push(<ArtistMusicDetails idx={i} />);
+    songComponents.push(<ArtistMusicDetails idx={i} key={i} />);
   }
 
   return (
     <>
       <ArtistDetails />
       {songComponents}
+      <UserSurveyForm />
     </>
   )
 }
