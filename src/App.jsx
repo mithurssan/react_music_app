b22186a6e33e695f17bd0a087e57e32c8e974aa7
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-05-15 12:27:37 +0100

    add artist name, music type and introduction paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,45 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const artist = {
+  name: "Eminem",
+  genre: "Hip-Hop",
+  imageUrl: "https://i.imgur.com/IE6Xlzt.jpeg",
+  description: "Marshall Bruce Mathers III (born October 17, 1972), known professionally as Eminem, is an American rapper, songwriter, and record producer. He is credited with popularizing hip hop in middle America and is critically acclaimed as one of the greatest rappers of all time. Eminem is one of the best-selling artists in music history, and easily one of rap's biggest crossover successes. He was the first white rapper since the Beastie Boys to garner both sales and critical respect, but his impact has exceeded this confining distinction. On sheer technical skills, Eminem is one of the greatest MCs of his generation: rapid, fluid, dexterous, and unpredictable, capable of pulling off long-form narratives or withering asides."
+};
+
+
+function ArtistDetails() {
+  return (
+    <>
+      <h1>{artist.name} ({artist.genre})</h1>
+      <div className="artist-info">
+        <img
+          className="avatar"
+          src={artist.imageUrl}
+          alt={`Photo of ${artist.name}`}
+        />
+        <p>{artist.description}</p>
+      </div>
+
+    </>
+  )
+}
+
+function ArtistMusicDetails() {
+  return (
+    <button>I'm a button</button>
+  )
+}
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ArtistDetails />
     </>
   )
 }
