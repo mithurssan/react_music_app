8313531feb9466fe914661c8ebda77a156cf977e
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-05-17 18:28:30 +0100

    cleanup, update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { NavBar } from './layout';
 import * as Pages from "./pages";
 import './App.css'
 
 function App() {
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const colours = ["#3d3d3d", "#383838", "#242424"];
+      const randomColour = colours[Math.floor(Math.random() * colours.length)];
+      document.body.style.backgroundColor = randomColour;
+    }, 5000)
+    return () => clearInterval(timer)
+  }, [])
+
   return (
     <div className="App">
       <Routes>
@@ -14,7 +24,6 @@ function App() {
           <Route path="/feedback" element={<Pages.Feedback />} />
         </Route>
       </Routes>
-
     </div>
   )
 }
